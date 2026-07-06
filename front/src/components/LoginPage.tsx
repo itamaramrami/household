946eb38c1ecc5945fee1e3b40979651567119624
itamar <ya0534516378@gmail.com>
2026-07-06 09:37:28 +0300

    fix build issues

--- front/src/components/LoginPage.tsx
+++ front/src/components/LoginPage.tsx
@@ -1,9 +1,7 @@
 import GoogleLoginButton from './GoogleLoginButton'; 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useGoogleLogin } from '@react-oauth/google';
 import { LoginFormData } from '../modules/LoginDate';
-import { CodeResponse } from '@react-oauth/google';
 
 
 const LoginPage = () => {
